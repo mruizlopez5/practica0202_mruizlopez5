3d15e230bf8de6534a39bfb8d637b5b39232f72e
Mark.os <mruizlopez5@educacion.navarra.es>
2024-11-11 15:55:48 +0100

    nueve

--- Ejercicio9.py
+++ Ejercicio9.py
@@ -4,3 +4,6 @@ el día, el mes y el año. Adaptar el programa anterior para
 que también funcione cuando el día o el mes se introduzcan 
 con un solo carácter."""
 
+fecha = input("introduce tu fecha de nacimiento en formato dd/mm/aa\n")
+fecha = fecha.split("/")
+print("dia: ",fecha[0],"\nmes: ",fecha[1],"\naño:")
